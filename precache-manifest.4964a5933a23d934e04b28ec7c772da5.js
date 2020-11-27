0b23b260275cf40d63a400bed3a250a370aa3143
Don Tay Xi Cheng <don.tay@shopback.com>
2020-11-27 11:18:15 +0800

    Updates

---
+++ precache-manifest.4964a5933a23d934e04b28ec7c772da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c11ae37450bbdcf6864bd66e20ab30",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "2bd44a1eadd50d3aaa5d",
+    "url": "/github-user-search/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "3deffa1a916169fd0ae0",
+    "url": "/github-user-search/static/js/2.01d8ef6c.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/github-user-search/static/js/2.01d8ef6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd44a1eadd50d3aaa5d",
+    "url": "/github-user-search/static/js/main.772ac29c.chunk.js"
+  },
+  {
+    "revision": "897cef59f97cc5596095",
+    "url": "/github-user-search/static/js/runtime-main.ca5a9141.js"
+  }
+]);
